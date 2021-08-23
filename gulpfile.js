9d74158ea6363d4097a59cf2a7dad181e9c62478
ojedam13 <m_ojeda17@hotmail.com>
2021-08-23 18:47:44 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass') (require('sass'));
+var autoprefixer = require('gulp-autoprefixer');
 
 function css() {
     return gulp
         .src('scss/app.scss')
         .pipe(autoprefixer({
-            overrideBrowserslist : ['last 2 versions'],
+            overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
         .pipe(sass({
-            outputStyle: 'expanded', // nested, compact, compressed
+            outputStyle: 'expanded',
         }))
-        .pipe( gulp.dest('css') );
+        .pipe(gulp.dest('css'));
 }
 
 function watchFiles() {
-    gulp.watch('scss/*.scss', css); 
+    gulp.watch('scss/*.scss', css);
     gulp.watch('index.html');
 }
 
 
-// Registrar funciones como tareas
-gulp.task( 'css', css );
-gulp.task('watch', gulp.parallel( watchFiles) );
+
+// registar funciones como tarea
+
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
+
+
+
